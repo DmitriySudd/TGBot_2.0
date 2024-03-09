76b5251a7dcc8f6494ab7f6842d47a0194cbeff5
DmitriySudd <dimsud1@gmail.com>
2024-03-10 00:54:19 +0400

    2 урок

--- bot/commands/__init__.py
+++ bot/commands/__init__.py
@@ -2,14 +2,12 @@ __all__ = ['register_user_commands', 'bot_commands']
 
 from aiogram import Router
 from aiogram.filters.command import CommandStart
+from aiogram.filters.command import Command
 
 from bot.commands.start import start
-
-bot_commands = (
-    ('start', 'Давайте начнем тест', 'Хорошая команда, чтобы начать работу с ботом'),
-    ('help', 'Помощь и справка', 'Поможет, если это будет нужно'),
-)
+from bot.commands.help import help_command
 
 
 def register_user_commands(router: Router) -> None:
-    router.message.register(start, CommandStart)
+    router.message.register(start, CommandStart())
+    router.message.register(help_command, Command(commands=['help']))
